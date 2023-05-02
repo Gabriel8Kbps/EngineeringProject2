e06342186513f42a9531684879bfcf50481e589d
Gabriel8Kbps <128176412+Gabriel8Kbps@users.noreply.github.com>
2023-05-02 01:37:12 -0300

    Ajuste Funções I

--- main.c
+++ main.c
@@ -6,44 +6,15 @@ int main()
 {
 
 
-
-    printf("=====Equipe=====\n\n");
-
-    char name1[]= "LUCAS ALEXSANDRO LEAL DOS SANTOS";
-    char name2[]= "GABRIEL DE JESUS DOS SANTOS COSTA";
-    char name3[]= "SAULO MATTHEUS RIBEIRO DE OLIVEIRA";
-    char name4[]= "JOSE HENRIQUE BARBOSA PENA";
-
-    printf(" %s\n %s\n %s\n %s\n\n", name1,name2, name3, name4);
-
-    printf("======Teste da Operacao de Soma========\n\n");
-        teste_soma();
-
-    printf("======Teste da Operacao de Subtracao========\n\n");
-        teste_subtracao();
-
-    printf("======Teste da Operacao Transposta======\n\n");
-        teste_transposta();
-
-    printf("======Teste da Conjugada========\n\n");
-        teste_conjugada();
-
-    printf("======Teste da Hermitiana========\n\n");
-        teste_hermitiana();
-
-//    printf("======Teste do Produto Matricial========\n\n");
-//        teste_produto_matricial();
-//
-
-
-//    printf("======Teste da Hermitiana========\n\n");
-//        teste_hermitiana();
-
-//    printf("======Teste do Produto Escalar========\n\n");
-//        teste_produto_escalar();
-
-//    printf("======Teste Geral========\n\n");
-//        teste_todos();
+    print_name();
+    teste_soma();
+    teste_subtracao();
+    teste_transposta();
+    teste_conjugada();
+    teste_hermitiana();
+    teste_produto_escalar();
+    teste_produto_matricial();
+    teste_todos();
 
 
     return 0;
