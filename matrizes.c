52d6adccff83ae6a279ba601541b06db016fcc4d
Gabriel8Kbps <128176412+Gabriel8Kbps@users.noreply.github.com>
2023-05-02 01:36:35 -0300

    Ajuste testes I

--- matrizes.c
+++ matrizes.c
@@ -3,7 +3,7 @@
 #include "matrizes.h"
 #define tam 3
 
-// MATRIZ TESTE, CADA ELEMENTO DA MATRIZ É UM VETOR
+// MATRIZ TESTE (CADA ELEMENTO DA MATRIZ É UM VETOR)
 struct ComplexNumber a[tam][tam] = { {{5,2},{7,-3},{9,1}   }  ,   {   {1,-6},{-5,3},{4,7}    }   ,   {   {2,5},{1,4},{-9,-3} }   };
 struct ComplexNumber b[tam][tam] = { {{7,-5},{3,-1},{1,-6} }    ,   {   {4,2},{-1,1},{2,9}   }   ,   {   {3,8},{2,4},{8,8}   }  };
 struct ComplexNumber c[tam][tam] = { {{1,-2},{-3,-3},{1,8}   }  ,   {    {7,3},{0,2},{7,1}   }   ,   {   {2,3},{9,2},{-1,-5} }    };
@@ -11,7 +11,7 @@ struct ComplexNumber d[tam][tam] = { {{6,-4},{-8,1},{3,1}  } ,   { {7,2},{1,9},{
 struct ComplexNumber result[tam][tam];
 
 // FUNCÕES
-
+int print_name();
 int soma(struct ComplexNumber a[][tam], struct ComplexNumber b[][tam], struct ComplexNumber result[][tam]);
 int teste_soma();
 
@@ -30,7 +30,21 @@ int teste_hermitiana();
 int produto_escalar(struct ComplexNumber a[][tam], struct ComplexNumber b[][tam], struct ComplexNumber result[][tam]);
 int teste_produto_escalar();
 
+int produto_matricial(struct ComplexNumber a[][tam], struct ComplexNumber b[][tam], struct ComplexNumber result[][tam]);
+int teste_produto_matricial();
 
+
+print_name()
+{
+    printf("=====Equipe=====\n\n");
+
+        char name1[]= "LUCAS ALEXSANDRO LEAL DOS SANTOS";
+        char name2[]= "GABRIEL DE JESUS DOS SANTOS COSTA";
+        char name3[]= "SAULO MATTHEUS RIBEIRO DE OLIVEIRA";
+        char name4[]= "JOSE HENRIQUE BARBOSA PENA";
+
+        printf(" %s\n %s\n %s\n %s\n\n", name1,name2, name3, name4);
+}
 
 int soma(struct ComplexNumber a[][tam], struct ComplexNumber b[][tam], struct ComplexNumber result[][tam])
 {
@@ -49,7 +63,7 @@ int soma(struct ComplexNumber a[][tam], struct ComplexNumber b[][tam], struct Co
 int teste_soma()
 {
     int i,j;
-
+    printf("======Teste da Operacao de Soma========\n\n");
     printf("Operando A:\n\n");
 
      for(i = 0;i < tam; i++)
@@ -149,7 +163,7 @@ int subtracao(struct ComplexNumber a[][tam], struct ComplexNumber b[][tam], stru
 int teste_subtracao()
 {
     int i,j;
-
+    printf("======Teste da Operacao de Subtracao========\n\n");
     printf("Operando A:\n\n");
 
     for(i = 0; i < tam; i++)
@@ -244,7 +258,8 @@ int transposta(struct ComplexNumber a[][tam],struct ComplexNumber result[][tam])
 }
 int teste_transposta()
 {
-  int i,j;
+    int i,j;
+    printf("======Teste da Operacao Transposta======\n\n");
     printf("Operando A:\n\n");
     for(i = 0; i < tam; i++)
         {
@@ -310,6 +325,7 @@ int teste_conjugada()
 
 
     int i,j;
+    printf("======Teste da Conjugada========\n\n");
     printf("Operando A:\n\n");
     for(i = 0; i < tam; i++)
         {
@@ -375,7 +391,7 @@ int hermitiana(struct ComplexNumber a[][tam], struct ComplexNumber b[][tam],stru
 }
 int teste_hermitiana()
 {
-
+    printf("======Teste da Hermitiana========\n\n");
     int i,j;
     printf("Operando A:\n\n");
     for(i = 0; i < tam; i++)
@@ -442,7 +458,7 @@ int produto_escalar(struct ComplexNumber a[][tam], struct ComplexNumber b[][tam]
 int teste_produto_escalar()
 {
     int i,j;
-
+    printf("======Teste do Produto Escalar========\n\n");
     printf("Operando A:\n\n");
     for(i = 0; i < tam; i++)
     {
@@ -500,14 +516,108 @@ int teste_produto_escalar()
     {
         for(j = 0; j < tam; j++)
             {
-                printf("\t(%.2f)",result[i][j].real + result[i][j].img);
+                printf("\t(%.2f)",result[i][j].real + result[i][j].img); //AQUI NÃO IMPLEMENTEI A SOMA REAL + IMG DIRETO NA FUNÇÃO
+            }
+        printf("\n");
+    }
+    printf("\n");
+}
+int produto_matricial(struct ComplexNumber a[][tam], struct ComplexNumber b[][tam], struct ComplexNumber result[][tam])
+{
+        int i,j;
+        for(i = 0 ; i < tam; i++)
+        {
+            for(j = 0; j < tam; j++)
+            {
+               result[i][j].real = a[i][j].real * b [j][i].real;
+               result[i][j].img = a[i][j].img * b [j][i].img;
+            }
+        }
+}
+
+int teste_produto_matricial()
+{
+    int i,j;
+    printf("======Teste do Produto Matricial========\n\n");
+    printf("Operando A:\n\n");
+    for(i = 0; i < tam; i++)
+    {
+        for(j = 0; j < tam; j++)
+            {
+                printf("\t%.2f",a[i][j].real);
+            }
+        printf("\n");
+    }
+    printf("\n");
+    printf("Operando B:\n\n");
+    for(i = 0; i < tam; i++)
+    {
+        for(j = 0; j < tam; j++)
+            {
+                printf("\t%.2f",b[i][j].real);
+            }
+        printf("\n");
+    }
+    printf("\n");
+    produto_matricial(a,b,result);
+    printf("Produto dos elementos de A e B:\n\n");
+    for(i = 0; i < tam; i++)
+    {
+        for(j = 0; j < tam; j++)
+            {
+                printf("\t%.2f",result[i][j].real);
+            }
+        printf("\n");
+    }
+     printf("\n");
+    printf("Operando C:\n\n");
+    for(i = 0; i < tam; i++)
+    {
+        for(j = 0; j < tam; j++)
+            {
+                printf("\t(%.2f) + (%.2fi)",c[i][j].real,c[i][j].img);
+            }
+        printf("\n");
+    }
+    printf("\n");
+    printf("Operando D:\n\n");
+    for(i = 0; i < tam; i++)
+    {
+        for(j = 0; j < tam; j++)
+            {
+                printf("\t(%.2f) + (%.2fi)",d[i][j].real,d[i][j].img);
+            }
+        printf("\n");
+    }
+    printf("\n");
+    produto_matricial(c,d,result);
+    printf("Produto Matricial de C * D:\n\n");
+    for(i = 0; i < tam; i++)
+    {
+        for(j = 0; j < tam; j++)
+            {
+                printf("\t%.2f",result[i][j].real+result[i][j].img); //AQUI NÃO IMPLEMENTEI A SOMA REAL + IMG DIRETO NA FUNÇÃO
             }
         printf("\n");
     }
     printf("\n");
 }
 
+teste_todos()
+{
+    printf("======Teste Geral========\n\n");
+    //MATRIZ PARA TESTE GERAL
+    struct ComplexNumber x[tam][tam] = { {{5,2},{7,-3},{9,1}   }  ,   {   {1,-6},{-5,3},{4,7}    }   ,   {   {2,5},{1,4},{-9,-3} }   };
+    struct ComplexNumber y[tam][tam] = { {{5,2},{7,-3},{9,1}   }  ,   {   {1,-6},{-5,3},{4,7}    }   ,   {   {2,5},{1,4},{-9,-3} }   };
 
-//teste_produto_matricial()
-//teste_todos();
+    //FUNÇÕES
+    soma(x,y,result);
+    teste_soma();
+    teste_subtracao();
+    teste_transposta();
+    teste_conjugada();
+    teste_hermitiana();
+    teste_produto_escalar();
+    teste_produto_matricial();
+}
 
