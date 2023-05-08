b1d70b4a2dee61f46abbefc49a2b4411420f5c72
Gabriel8Kbps <128176412+Gabriel8Kbps@users.noreply.github.com>
2023-05-08 01:21:43 -0300

    Modelo com Base em Ponteiros

--- matrizes.h
+++ matrizes.h
@@ -1,12 +1,33 @@
-#ifndef MATRIZES_H
-#define MATRIZES_H
+#ifndef MATRIZES
+#define MATRIZES
+#include "matrizes.c"
 
-typedef struct ComplexNumber
+typedef struct
 {
-
     double real;
     double img;
+}Complex;
 
-};
+Complex** allocateComplexMatrix (int linhas, int colunas)
+{
+    int i;
+    Complex** matrix;
+    matrix = (Complex **) malloc(linhas * sizeof(Complex*));
+    if (matrix == NULL) {
+        printf("Memory Allocation Failed\n");
+        exit(1);
+    }
+    for(i = 0; i < colunas; i++){
+        matrix[i] = (int *) malloc(colunas * sizeof(Complex));
+        if (matrix[i] == NULL){
+            printf("Memory Allocation Failed\n");
+            exit(1);
+        }
+    }
+}
+
+print_name();
+//soma(a,b);
+//teste_soma(a);
 
 #endif
